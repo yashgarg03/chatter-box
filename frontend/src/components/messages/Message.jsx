8e7fdb16a7e80906e8cf419cce107b6c337e30a6
Yash Garg <yashgarg@Etechaces.com>
2024-03-08 13:41:06 +0530

    production ready

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -9,6 +9,7 @@ const Message = ({message}) => {
   const chatClassName = !fromMe ? 'chat-start' : 'chat-end';
   const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
   const bubbleBgColor = fromMe ? "bg-blue-500" : "bg-slate-900";
+  const shakeClass = message.shouldShake ? "shake" : ""; 
   return (
     <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
@@ -19,7 +20,7 @@ const Message = ({message}) => {
           />
         </div>
       </div>
-      <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
       <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>{extractTime(message.createdAt)}</div>
     </div>
   );
--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext"
 import useConversation from "../zustand/useConversation";
+import notificationSound from "../assets/sound/notification.mp3"
 
 const useListenMessages = () => {
     const { socket } = useSocketContext();
@@ -8,6 +9,9 @@ const useListenMessages = () => {
 
     useEffect(() => {
         socket?.on("newMessage", (newMessage) => {
+            newMessage.shouldShake = true;
+            const sound = new Audio(notificationSound);
+            sound.play();
             setMessages([...messages, newMessage]);
         })
 
