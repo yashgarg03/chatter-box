e2e54d6914303f2f18269a6b85847d300e137adb
Yash Garg <gargyash900@gmail.com>
2024-03-08 16:17:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://chatter-box-eosin.vercel.app/',
         changeOrigin: true
       }
     }
