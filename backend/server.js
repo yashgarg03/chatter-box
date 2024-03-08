e67e962671939af3722aee3b727f59ddb263425f
Yash Garg <yashgarg@Etechaces.com>
2024-03-08 13:41:35 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
-import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
-import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
+import messageRoutes from './routes/message.routes.js';
 
 import connectToMongoDB from './db/ConnectToMongoDB.js';
 import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 8000
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes) 
-app.use("/api/message", messageRoutes) 
-app.use("/api/users", userRoutes) 
+app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
+app.use("/api/users", userRoutes)
 
-// app.get('/', (req, res) => {
-//     // root route http://localhost:5000/
-//     res.send('Hello World!')
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
